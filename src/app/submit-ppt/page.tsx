bc488b9862ef7d717b39e1b52e590f1434467dfa
ANKAN DALUI <143159255+ankandalui@users.noreply.github.com>
2025-09-30 13:17:07 +0530

    ppt submission preview added

--- src/app/submit-ppt/page.tsx
+++ src/app/submit-ppt/page.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
 
 function ClientRedirect() {
   const formUrl =
-    "https://docs.google.com/forms/d/e/1FAIpQLSe5opmZjIf1LM_9lM8m1f2kqmlSOFWdbI3uh1adsK1AMpgqSA/viewform?usp=sharing&ouid=105135097797986051672&rtpof=true&sd=true";
+    "https://docs.google.com/forms/d/e/1FAIpQLSe5opmZjIf1LM_9lM8m1f2kqmlSOFWdbI3uh1adsK1AMpgqSA/viewform?usp=sharing&ouid=105135097797986051672";
 
   return (
     <>
