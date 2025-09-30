6774ef070b555ffcf9e044ab0a30b254017b53ab
ANKAN DALUI <143159255+ankandalui@users.noreply.github.com>
2025-09-30 12:16:46 +0530

    ppt submission link added

---
+++ src/app/ppt-submission/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+export default function PPTSubmissionPage() {
+  redirect(
+    "https://docs.google.com/forms/d/e/1FAIpQLSe5opmZjIf1LM_9lM8m1f2kqmlSOFWdbI3uh1adsK1AMpgqSA/viewform?usp=sharing&ouid=105135097797986051672&rtpof=true&sd=true"
+  );
+}
